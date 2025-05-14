824fd1c6a7ab9490ffda0a8eec675e1084e70575
SpaceBun <spacebuneth@gmail.com>
2025-05-13 23:19:13 -0400

    Users can now upload PDF files, React will convert the file to URLBase64, Added Bitcoin Whitepaper in Public folder to help test. Note there seems to be a state bug where initial file upload reads as null

--- pdf-2-base64/src/app/components/upload.js
+++ pdf-2-base64/src/app/components/upload.js
@@ -1,9 +1,53 @@
 'use client'
 
+import {useState} from 'react';
+
+
 export default function Upload() {
+    const [fileContent, SetFileContent] = useState(null);
+
+    function handleFile() {
+       
+        const file = document.getElementById("pdf-file").files[0];
+        const reader = new FileReader();
+
+
+        reader.addEventListener(
+            "load",
+            () => {
+                SetFileContent(reader.result);
+            },
+            false,
+        );
+
+        reader.addEventListener(
+            "loadend",
+            () => {
+                EncodePDF();
+            },
+            false,
+        );
+
+        if (file) {
+            reader.readAsDataURL(file); // when done reading pdf trigger event
+        }
+
+
+    };
+
+    function EncodePDF() {
+        const buffer = fileContent;
+        console.log(buffer)
+    };
+
+    function DecodePDF() {
+
+    }
+    
+
     return (
         <>
-        input tag goes here
+            <input type="file" id="pdf-file" name="pdf-file" accept=".pdf" onChange={handleFile}/>
         </>
     );
 }
